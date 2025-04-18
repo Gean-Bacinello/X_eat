b47ab304a624b2de6a3b4ecfd5088dcd7f42038d
Gean Correa Bacinello <geanbacinello197@gmail.com>
2025-04-18 19:01:10 -0300

    feat: adicionar PacienteController com métodos CRUD

--- app/Http/Controllers/PacienteController.php
+++ app/Http/Controllers/PacienteController.php
@@ -2,65 +2,113 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Paciente;
 use Illuminate\Http\Request;
+use PHPUnit\Metadata\Uses;
 
 class PacienteController extends Controller
-{
+{   
+    // ===================[ CRUD - EXIBIR ]===================
     /**
-     * Display a listing of the resource.
+     * Exibe os pacientes cadastrados no banco de dados
+     * Com a função de pesquisa -> model -> Paciente.php
+     * Paginação exibindo apenas 6 pacientes por pagina 
+     * 
+     * @param Request $request
+     * @return void
      */
-    public function index()
+    public function index(Request $request)
     {
-        //
-        return view('pacientes.index');
-       
+       $pacientes = Paciente::when($request->filled('search'), function ($query) use ($request){
+        $query->buscar($request->search);
+       })->paginate(6)->appends($request->query());
+
+       return view('pacientes.index', [
+        'pacientes' => $pacientes
+       ]);
     }
 
+    // ===================[ CRUD - CRIAR ]===================
     /**
-     * Show the form for creating a new resource.
+     * Rota para o metodo create
+     * mostra o formulario de cadastro de paciente
+     *
+     * @return void
      */
     public function create()
     {
-        //
+        return view('pacientes.create');
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
+    // ===================[ CRUD - ARMAZENAR ]===================
+   /**
+    * Função Responsável por enviar o formulário de cadastro
+    * armazena os dados do paciente no banco de dados 
+    *
+    * @param Request $request
+    * @return void
+    */
     public function store(Request $request)
     {
-        //
+        $paciente = $request->except('_token');
+        Paciente::create($paciente);
+
+        return redirect()->route('pacientes.index')->with('msg', 'Paciente Cadastrado com sucesso!');
     }
 
+    // ===================[ CRUD - EXIBIR{ID} ]===================
     /**
-     * Display the specified resource.
+     * Exibe as informações de um paciente 
+     * ultiliza o id do paciente
+     *
+     * @param string $id
+     * @return void
      */
     public function show(string $id)
     {
-        //
+        $paciente = Paciente::findOrFail($id);  //Busca a lista total de pacientes no banco
+        return view('pacientes.show', [
+            'paciente' => $paciente
+        ]);
     }
 
+    // ===================[ CRUD - EDITAR ]===================
     /**
-     * Show the form for editing the specified resource.
+     * captura as informações salvas do formulario 
+     * exibe as respectivas informações do paciente
      */
     public function edit(string $id)
     {
-        //
+        $paciente = Paciente::find($id);
+
+        return view('pacientes.edit', [
+            'paciente' => $paciente 
+        ]);
     }
 
+    // ===================[ CRUD - ATUALIZAR ]===================
     /**
-     * Update the specified resource in storage.
+     * Atualiza as informações do paciente através do formulário
+     * ultiliza o id do paciente
      */
-    public function update(Request $request, string $id)
+    public function update(Request $request, int $id)
     {
-        //
+        $paciente = Paciente::find($id);
+        $paciente->update($request->all());
+
+        return redirect()->route('pacientes.index')->with('msg', 'Paciente atualizado com sucesso!');
     }
 
+    // ===================[ CRUD - EXCLUIR ]===================
     /**
-     * Remove the specified resource from storage.
+     * Exclui as informações do paciente atravez do formulario
+     * ultiliza o id do paciente
      */
     public function destroy(string $id)
     {
-        //
+        $paciente = Paciente::findOrFail($id);
+        $paciente->delete();
+
+        return redirect()->route('pacientes.index')->with('msg', 'Paciente excluido com sucesso!');
     }
 }
