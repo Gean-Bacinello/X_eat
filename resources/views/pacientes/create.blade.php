abf0ddae44a522948a605fa9500c5287b4795233
Gean Correa Bacinello <geanbacinello197@gmail.com>
2025-04-19 19:44:07 -0300

    feat:(pacientes) adicionar campo * para campos obrigatorios

--- resources/views/pacientes/create.blade.php
+++ resources/views/pacientes/create.blade.php
@@ -13,15 +13,17 @@
 
                     <div class="row">
                         <div class="col-md-4 mb-3">
-                            <label for="Nome Paciente" class="form-label">Nome do Paciente</label>
+                            <label for="Nome Paciente" class="form-label"><span class="text-danger">*</span>Nome do
+                                Paciente</label>
                             <input type="text" class="form-control" id="Nome" name="Nome" required>
                         </div>
                         <div class="col-md-4 mb-3">
-                            <label for="Data Nascimento" class="form-label">Data Nascimento</label>
+                            <label for="Data Nascimento" class="form-label"><span class="text-danger">*</span>Data
+                                Nascimento</label>
                             <input type="date" class="form-control" id="Data_Nascimento" name="Data_Nascimento" required>
                         </div>
                         <div class="col-md-4 mb-3">
-                            <label for="Sexo" class="form-label">Sexo</label>
+                            <label for="Sexo" class="form-label"><span class="text-danger">*</span>Sexo</label>
                             <select class="form-control" id="Sexo" name="Sexo" required>
                                 <option value="Masculino">Masculino</option>
                                 <option value="Feminino">Feminino</option>
@@ -30,28 +32,29 @@
                         </div>
 
                         <div class="col-md-4 mb-3">
-                            <label for="Peso" class="form-label">Peso</label>
+                            <label for="Peso" class="form-label"><span class="text-danger">*</span>Peso</label>
                             <input type="number" class="form-control" id="Peso" name="Peso" step="0.01"
                                 required>
                         </div>
                         <div class="col-md-4 mb-3">
-                            <label for="Altura" class="form-label">Altura</label>
+                            <label for="Altura" class="form-label"><span class="text-danger">*</span>Altura</label>
                             <input type="number" class="form-control" id="Altura" name="Altura" step="0.01"
                                 required>
                         </div>
                         <div class="col-md-4 mb-3">
-                            <label for="Email" class="form-label">Email</label>
+                            <label for="Email" class="form-label"><span class="text-danger">*</span>Email</label>
                             <input type="text" class="form-control" id="Email" name="Email" required>
                         </div>
                         <div class="col-md-4 mb-3">
-                            <label for="Senha" class="form-label">Senha</label>
+                            <label for="Senha" class="form-label"><span class="text-danger">*</span>Senha</label>
                             <input type="text" class="form-control" id="Senha" name="Senha"
                                 pattern="(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{8,}"
                                 title="A senha deve conter pelo menos 8 caracteres, uma letra maiúscula, uma letra minúscula e um número."
                                 required>
                         </div>
                         <div class="col-md-4 mb-3">
-                            <label for="Nutricionista" class="form-label">Nutricionista Responsavel</label>
+                            <label for="Nutricionista" class="form-label"><span class="text-danger">*</span>Nutricionista
+                                Responsavel</label>
                             <input type="number" class="form-control" id="Nutricionista_ID" name="Nutricionista_ID"
                                 step="1" min="0" max="100" placeholder="ID"
                                 title="Identificador do nutricionista responsavel" required>
--- resources/views/pacientes/edit.blade.php
+++ resources/views/pacientes/edit.blade.php
@@ -14,17 +14,19 @@
 
                     <div class="row">
                         <div class="col-md-4 mb-3">
-                            <label for="Nome paciente" class="form-label">Nome do Paciente</label>
+                            <label for="Nome paciente" class="form-label"><span class="text-danger">*</span>Nome do
+                                Paciente</label>
                             <input type="text" class="form-control" id="Nome" name="Nome"
                                 value="{{ $paciente->Nome }}" required>
                         </div>
                         <div class="col-md-4 mb-3">
-                            <label for="Data Nascimento" class="form-label">Data Nascimento</label>
+                            <label for="Data Nascimento" class="form-label"><span class="text-danger">*</span>Data
+                                Nascimento</label>
                             <input type="date" class="form-control" id="Data_Nascimento" name="Data_Nascimento"
                                 value="{{ $paciente->Data_Nascimento }}" required>
                         </div>
                         <div class="col-md-4 mb-3">
-                            <label for="Sexo" class="form-label">Sexo</label>
+                            <label for="Sexo" class="form-label"><span class="text-danger">*</span>Sexo</label>
                             <select class="form-control" id="Sexo" name="Sexo" required>
                                 <option value="Masculino" {{ $paciente->Sexo == 'Masculino' ? 'selected' : '' }}>Masculino
                                 </option>
@@ -35,29 +37,30 @@
                         </div>
 
                         <div class="col-md-4 mb-3">
-                            <label for="Peso" class="form-label">Peso</label>
+                            <label for="Peso" class="form-label"><span class="text-danger">*</span>Peso</label>
                             <input type="number" class="form-control" id="Peso" name="Peso"
                                 value="{{ $paciente->Peso }}" step="0.01" required>
                         </div>
                         <div class="col-md-4 mb-3">
-                            <label for="Altura" class="form-label">Altura</label>
+                            <label for="Altura" class="form-label"><span class="text-danger">*</span>Altura</label>
                             <input type="number" class="form-control" id="Altura" name="Altura"
                                 value="{{ $paciente->Altura }}" step="0.01" required>
                         </div>
                         <div class="col-md-4 mb-3">
-                            <label for="Email" class="form-label">Email</label>
+                            <label for="Email" class="form-label"><span class="text-danger">*</span>Email</label>
                             <input type="text" class="form-control" id="Email" name="Email"
                                 value="{{ $paciente->Email }}" required>
                         </div>
                         <div class="col-md-4 mb-3">
-                            <label for="Senha" class="form-label">Senha</label>
+                            <label for="Senha" class="form-label"><span class="text-danger">*</span>Senha</label>
                             <input type="text" class="form-control" id="Senha" name="Senha"
                                 value="{{ $paciente->Senha }}" pattern="(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{8,}"
                                 title="A senha deve conter pelo menos 8 caracteres, uma letra maiúscula, uma letra minúscula e um número."
                                 required>
                         </div>
                         <div class="col-md-4 mb-3">
-                            <label for="Nutricionista" class="form-label">Nutricionista Responsavel</label>
+                            <label for="Nutricionista" class="form-label"><span class="text-danger">*</span>Nutricionista
+                                Responsavel</label>
                             <input type="number" class="form-control" id="Nutricionista_ID" name="Nutricionista_ID"
                                 value="{{ $paciente->Nutricionista_ID }}" step="1" min="0" max="100"
                                 placeholder="ID" title="Identificador do nutricionista responsavel" required>
